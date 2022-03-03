0d9c81acd4ea100b7a436799e262dba4d4dcd410
OjasBhanarkar <ojasbhanarkar7874@gmail.com>
2022-03-03 12:13:20 +0530

    fifth commit

--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.css']
 })
-export class ListingComponent implements OnInit {
+export class ListingComponent {
 
-  constructor() { }
+  user$ = this.authService.user$;
+  logout$!: Observable<any>;
 
-  ngOnInit(): void {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) { }
+
+  logout(): void {
+    this.logout$ = this.authService
+      .logout()
+      .pipe(
+        tap(() => this.router.navigateByUrl('.login'))
+      )
   }
 
 }
