b7d2777c23aafe7354082f91190b08d9a86543e8
OjasBhanarkar <ojasbhanarkar7874@gmail.com>
2022-03-07 08:42:38 +0530

    7 march commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatCardModule} from '@angular/material/card'; 
-import {MatDividerModule} from '@angular/material/divider'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'
+import { AngularFireModule } from '@angular/fire/compat';
+
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ListingComponent } from './components/listing/listing.component';
@@ -35,8 +39,13 @@ import { ListingComponent } from './components/listing/listing.component';
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-listing',
@@ -15,6 +16,7 @@ export class ListingComponent {
   logout$!: Observable<any>;
 
   constructor(
+    private af:AngularFireStorage,
     private readonly authService: AuthService,
     private readonly router: Router
   ) { }
@@ -25,6 +27,18 @@ export class ListingComponent {
       .pipe(
         tap(() => this.router.navigateByUrl('.login'))
       )
+  }
+
+  path:String | undefined
+
+  upload($event: any){
+    this.path = $event.target.files[0]
+  }
+
+  uploadImage(){
+    console.log(this.path);
+
+    this.af.upload("/files"+Math.random()+this.path, this.path);
   }
 
 }
